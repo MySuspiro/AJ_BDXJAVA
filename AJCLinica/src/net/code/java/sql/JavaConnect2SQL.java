d36bfb8a692c03b412b664fc9a30516657a0ba43
Antho <Antho@BAMBIIIV3>
2024-07-26 10:38:04 -0400

    java conect auun no funciona as intended

--- AJCLinica/src/net/code/java/sql/JavaConnect2SQL.java
+++ AJCLinica/src/net/code/java/sql/JavaConnect2SQL.java
@@ -3,42 +3,51 @@ package net.code.java.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Date;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import jdk.internal.dynalink.beans.StaticClass;
+
 import java.sql.ResultSet;
 
 public class JavaConnect2SQL {
 
-
+	private static String connectionUrl =
+            "jdbc:sqlserver://192.168.100.118:1433;"
+                    + "database=gestion_paquetes;"
+                    + "user=an.rosario;" //TU USER
+                    + "password=Eict@2024;" //TU CLAVE
+                    + "encrypt=true;"
+                    + "trustServerCertificate=true;"
+                    + "loginTimeout=30;";
+	
     public static void main(String[] args) {
+    	
+        String selectSql = "SELECT * FROM lugar";
+        Connectionsql(selectSql);
 
-        String connectionUrl =
-                "jdbc:sqlserver://192.168.100.118:1433;"
-                        + "database=gestion_paquetes;"
-                        + "user=an.rosario;" //TU USER
-                        + "password=Eict@2024;" //TU CLAVE
-                        + "encrypt=true;"
-                        + "trustServerCertificate=true;"
-                        + "loginTimeout=30;";
-        
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl);
-             Statement statement = connection.createStatement()) {
-
-            String selectSql = "SELECT * FROM lugar";
-            ResultSet resultSet = statement.executeQuery(selectSql);
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id_lugar");
-                String nombre = resultSet.getString("nombre_lugar");
-                System.out.println("ID: " + id + ", Nombre: " + nombre);
-            }
-
-        } 
-        
-        catch (Exception e) {
-                System.out.println("error");
-                e.printStackTrace();
-        }
-        
     }
+
+	public static void Connectionsql(String selectSql) {
+		// TODO Auto-generated method stub
+		try (Connection connection = DriverManager.getConnection(connectionUrl);
+	             Statement statement = connection.createStatement()) {
+
+	            ResultSet resultSet = statement.executeQuery(selectSql);
+
+	            while (resultSet.next()) {
+	                int id = resultSet.getInt("id_lugar");
+	                String nombre = resultSet.getString("nombre_lugar");
+	                System.out.println("ID: " + id + ", Nombre: " + nombre);
+	            }
+
+	        } 
+	        
+	        catch (Exception e) {
+	                System.out.println("error");
+	                e.printStackTrace();
+	        }
+	}
 
 }
